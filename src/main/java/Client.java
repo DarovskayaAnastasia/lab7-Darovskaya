81dd2c982de032762f4a47dda3f7e8c6cae7cddb
Darovskaya Anastasia <stiltskenx@gmail.com>
2020-01-06 21:14:47 +0000

    Scripted auto-commit on change (2020-01-06 21:14:47) by gitwatch.sh

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,12 +1,21 @@
 import org.zeromq.SocketType;
 import org.zeromq.ZMQ;
 
+import java.util.Scanner;
+
 public class Client {
     public static void main(String[] args) {
+        System.out.println("Client started...");
         ZMQ.Context context = ZMQ.context(1);
 
         //socket to talk to server
         ZMQ.Socket requester = context.socket((SocketType.REQ));
-        requester.connect("tcp://localhost:5559")
+        requester.connect("tcp://localhost:5559");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Client ready");
+
+        //loop
+        while
     }
 }
