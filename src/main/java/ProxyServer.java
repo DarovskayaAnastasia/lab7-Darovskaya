f1d171cf387eb2ef429d76d8b3dedecf8db93b06
Darovskaya Anastasia <stiltskenx@gmail.com>
2020-01-12 13:03:08 +0000

    Scripted auto-commit on change (2020-01-12 13:03:08) by gitwatch.sh

--- src/main/java/ProxyServer.java
+++ src/main/java/ProxyServer.java
@@ -1,5 +1,4 @@
 import org.zeromq.*;
-//import zmq.poll.Poller;
 
 public class ProxyServer {
 
@@ -7,26 +6,30 @@ public class ProxyServer {
     public static void main(String[] args) {
         System.out.println("(ProxyServer message): Launch Proxy...");
 
-        ZContext context = new ZСontext();
+        ZMQ.Context context = ZMQ.context(0);
 
 //        Socket facing clients
-        ZMQ.Socket frontend = context.createSocket(SocketType.ROUTER);
+        ZMQ.Socket frontend = context.socket(SocketType.ROUTER);
         frontend.bind("tcp://localhost:5559");
 
 //        Socket facing services
-        ZMQ.Socket backend = context.createSocket(SocketType.ROUTER);
+        ZMQ.Socket backend = context.socket(SocketType.ROUTER);
         backend.bind("tcp:/localhost:5560");
 
 //        ZMQ.proxy(frontend, backend, null);
 
 //        Start the proxy
-        ZMQ.Poller poller = context.createPoller(2);
+        ZMQ.Poller poller = context.poller(2);
         poller.register(frontend, ZMQ.Poller.POLLIN);
         poller.register(backend, ZMQ.Poller.POLLIN);
         System.out.println("(ProxyServer message): Proxy has been started");
 
 //        Endless loop
-        while ()
+        while (!Thread.currentThread().isInterrupted()) {
+            poller.poll();
+//            remove storages
+
+        }
 
 
 //        We never get here but clean up anyhow
