af0a531ab16e803422b430c8dac8e0c25dbfe311
Darovskaya Anastasia <stiltskenx@gmail.com>
2020-01-12 12:46:36 +0000

    Scripted auto-commit on change (2020-01-12 12:46:36) by gitwatch.sh

--- src/main/java/ProxyServer.java
+++ src/main/java/ProxyServer.java
@@ -1,5 +1,5 @@
 import org.zeromq.*;
-import zmq.poll.Poller;
+//import zmq.poll.Poller;
 
 public class ProxyServer {
 
@@ -9,22 +9,27 @@ public class ProxyServer {
 
         ZContext context = new ZСontext();
 
-        // Socket facing clients
+//        Socket facing clients
         ZMQ.Socket frontend = context.createSocket(SocketType.ROUTER);
         frontend.bind("tcp://localhost:5559");
 
-        //Socket facing services
+//        Socket facing services
         ZMQ.Socket backend = context.createSocket(SocketType.ROUTER);
         backend.bind("tcp:/localhost:5560");
 
-        System.out.println("(ProxyServer message): Proxy has been started");
-
-//        // Start the proxy
 //        ZMQ.proxy(frontend, backend, null);
 
-        Poller poller = context.createPoller(2);
+//        Start the proxy
+        ZMQ.Poller poller = context.createPoller(2);
+        poller.register(frontend, ZMQ.Poller.POLLIN);
+        poller.register(backend, ZMQ.Poller.POLLIN);
+        System.out.println("(ProxyServer message): Proxy has been started");
 
-        // We never get here but clean up anyhow
+//        Endless loop
+        while ()
+
+
+//        We never get here but clean up anyhow
         frontend.close();
         backend.close();
         context.term();
