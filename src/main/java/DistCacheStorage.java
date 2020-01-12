c28a3d0380d950eb57dc54dcecf56f04de4797c1
Darovskaya Anastasia <stiltskenx@gmail.com>
2020-01-12 14:10:37 +0000

    Scripted auto-commit on change (2020-01-12 14:10:37) by gitwatch.sh

--- src/main/java/DistCacheStorage.java
+++ src/main/java/DistCacheStorage.java
@@ -3,6 +3,12 @@ import org.zeromq.ZMQ;
 public class DistCacheStorage {
 
     public static void main(String[] args) {
-        if (args.length < 2)
+        if (args.length < 2) {
+            System.err.println("(DistCacheStorage message): ERROR, incorrect number of arguments");
+            return;
+        }
+
+        ZMQ.Context context = ZMQ.context(0);
+        
     }
 }
