be50db385d512edcc0579e98dbd9d9c7eb08f0af
Darovskaya Anastasia <stiltskenx@gmail.com>
2020-01-13 20:33:50 +0000

    Scripted auto-commit on change (2020-01-13 20:33:50) by gitwatch.sh

--- src/main/java/DistCacheStorage.java
+++ src/main/java/DistCacheStorage.java
@@ -43,7 +43,7 @@ public class DistCacheStorage {
                 }
 
                 if (command.getCommandType().equals(Command.SET_TYPE)) {
-                               
+
                 }
             }
         }
