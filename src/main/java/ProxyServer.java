5316f28913b4234434aea3bd59115f28e56220b0
Mikhail Apakhov <stiltskenx@gmail.com>
2020-01-20 02:55:16 +0300

    Scripted auto-commit on change (2020-01-20 02:55:16) by gitwatch.sh

--- src/main/java/ProxyServer.java
+++ src/main/java/ProxyServer.java
@@ -98,7 +98,7 @@ public class ProxyServer {
 
                 if (command.typeCheck(Command.CONNECT_TYPE)) {
                     log.info("new cache by cmd", command);
-                    storages.add(new StorageInfo(id, command.getBegin(), command.getEnd(), ))
+                    storages.add(new StorageInfo(id, command.getBegin(), command.getEnd(), System.currentTimeMillis()));
                 }
             }
 
