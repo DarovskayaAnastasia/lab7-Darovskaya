948c713384b4f4acf4e5d288d9817f29b5697478
Mikhail Apakhov <stiltskenx@gmail.com>
2020-01-20 00:45:22 +0300

    Scripted auto-commit on change (2020-01-20 00:45:22) by gitwatch.sh

--- src/main/java/Logger.java
+++ src/main/java/Logger.java
@@ -1,11 +1,11 @@
 public class Logger {
     private String prefix;
 
-    public Logger(String prefix){
-        this.prefix = prefix+":";
+    public Logger(String prefix) {
+        this.prefix = prefix + ":";
     }
 
-    public void log(String ... strs) {
+    public void log(String... strs) {
         StringBuilder sb = new StringBuilder(prefix);
         for (String str : strs) {
             sb.append(" ").append(str);
